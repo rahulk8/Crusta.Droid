43f2f62835c097b758f44632b9b6640166a690f3
Anmol Gautam <tarptaeya@gmail.com>
2019-08-20 13:46:05 +0530

    add option for internal proxy

--- app/src/main/java/com/crustabrowser/android/WebViewClient.kt
+++ app/src/main/java/com/crustabrowser/android/WebViewClient.kt
@@ -3,6 +3,7 @@ package com.crustabrowser.android
 import android.content.SharedPreferences
 import android.graphics.Bitmap
 import android.os.AsyncTask
+import android.webkit.CookieManager
 import android.webkit.WebResourceRequest
 import android.webkit.WebResourceResponse
 import android.webkit.WebView
@@ -10,6 +11,11 @@ import androidx.core.view.isVisible
 import androidx.preference.PreferenceManager
 import com.crustabrowser.android.adblock.Adblocker
 import com.crustabrowser.android.history.History
+import java.io.ByteArrayInputStream
+import java.net.HttpURLConnection
+import java.net.InetSocketAddress
+import java.net.Proxy
+import java.net.URL
 
 class WebViewClient : android.webkit.WebViewClient() {
     var preferences: SharedPreferences? = null
@@ -39,7 +45,35 @@ class WebViewClient : android.webkit.WebViewClient() {
         }
 
         val uri = request?.url
-        return if (preferences!!.getBoolean("adblock", true) && Adblocker.contains(uri)) Adblocker.emptyResponse()
-               else super.shouldInterceptRequest(view, request)
+        if (preferences!!.getBoolean("adblock", true) && Adblocker.contains(uri)) {
+            return Adblocker.emptyResponse()
+        }
+
+        if (preferences!!.getBoolean("proxy", false)) {
+            try {
+                val url = URL(uri.toString())
+                val host = preferences!!.getString("proxy_host", "localhost")
+                val port = preferences!!.getInt("proxy_port", 8000)
+
+                val proxy = Proxy(Proxy.Type.HTTP, InetSocketAddress(host, port))
+                HttpURLConnection.setFollowRedirects(true)
+                val connection = url.openConnection(proxy) as HttpURLConnection
+                connection.instanceFollowRedirects = true
+
+                val cookies = CookieManager.getInstance().getCookie(uri.toString())
+                connection.addRequestProperty("Cookie", cookies)
+                connection.addRequestProperty("User-Agent", view?.settings?.userAgentString)
+
+                val type = connection.contentType ?: "text/plain"
+                val encoding = connection.contentEncoding ?: "utf-8"
+                val data = connection.inputStream.bufferedReader().readText()
+
+                return WebResourceResponse(type, encoding, ByteArrayInputStream(data.toByteArray()))
+            } catch (e: Exception) {
+                return WebResourceResponse("text/plain", "utf-8", ByteArrayInputStream("Proxy Error".toByteArray()))
+            }
+
+        }
+        return super.shouldInterceptRequest(view, request)
     }
 }
